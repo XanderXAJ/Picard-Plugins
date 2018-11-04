83250012eb0ccd131059ad2c922be43074315463
Alex Palmer <xanderx@xanderx.com>
2018-11-04 14:31:06 +0000

    Update plugins for Picard 2.0

--- removeartiststags.py
+++ removeartiststags.py
@@ -1,8 +1,8 @@
 PLUGIN_NAME = 'Remove Artists Tags'
 PLUGIN_AUTHOR = 'Alex Palmer'
 PLUGIN_DESCRIPTION = '''Removes artists and albumartists tags.'''
-PLUGIN_VERSION = "1.0"
-PLUGIN_API_VERSIONS = ["0.16"]
+PLUGIN_VERSION = "1.1"
+PLUGIN_API_VERSIONS = ["0.16", "1.0", "2.0"]
 
 from picard.metadata import register_track_metadata_processor
 
--- removesorttags.py
+++ removesorttags.py
@@ -2,7 +2,7 @@ PLUGIN_NAME = 'Remove Sort Tags'
 PLUGIN_AUTHOR = 'Alex Palmer'
 PLUGIN_DESCRIPTION = '''Removes albumartistsort, albumsort, artistsort, composersort and titlesort tags.'''
 PLUGIN_VERSION = "1.0"
-PLUGIN_API_VERSIONS = ["0.16"]
+PLUGIN_API_VERSIONS = ["0.16", "1.0", "2.0"]
 
 from picard.metadata import register_track_metadata_processor
 
