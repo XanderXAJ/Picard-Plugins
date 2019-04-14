438f72ebb996caaaecddc59bfa71860c05cc9b40
Alex Palmer <xanderx@xanderx.com>
2019-04-14 13:34:34 +0100

    Rename procedure to match file

--- removeartiststags.py
+++ removeartiststags.py
@@ -6,10 +6,10 @@ PLUGIN_API_VERSIONS = ["0.16", "1.0", "2.0"]
 
 from picard.metadata import register_track_metadata_processor
 
-def remove_sort_tags(tagger, metadata, release, track):
+def remove_artists_tags(tagger, metadata, release, track):
     TAGS = ['artists', 'albumartists']
     for tag in TAGS:
         if tag in metadata:
             del metadata[tag]
 
-register_track_metadata_processor(remove_sort_tags)
+register_track_metadata_processor(remove_artists_tags)
