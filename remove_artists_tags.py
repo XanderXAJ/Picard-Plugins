16dda99cfcff95f722b1152a3d10696f2e48ddef
Alex Palmer <xanderx@xanderx.com>
2019-04-14 13:38:43 +0100

    Move constants out of procedures to avoid unnecessary recreation

--- remove_artists_tags.py
+++ remove_artists_tags.py
@@ -4,10 +4,11 @@ PLUGIN_DESCRIPTION = '''Removes artists and albumartists tags.'''
 PLUGIN_VERSION = "1.1"
 PLUGIN_API_VERSIONS = ["0.16", "1.0", "2.0"]
 
+TAGS = ['artists', 'albumartists']
+
 from picard.metadata import register_track_metadata_processor
 
 def remove_artists_tags(tagger, metadata, release, track):
-    TAGS = ['artists', 'albumartists']
     for tag in TAGS:
         if tag in metadata:
             del metadata[tag]
--- remove_sort_tags.py
+++ remove_sort_tags.py
@@ -4,10 +4,11 @@ PLUGIN_DESCRIPTION = '''Removes albumartistsort, albumsort, artistsort, composer
 PLUGIN_VERSION = "1.0"
 PLUGIN_API_VERSIONS = ["0.16", "1.0", "2.0"]
 
+TAGS = ['albumartistsort', 'albumsort', 'artistsort', 'composersort', 'titlesort']
+
 from picard.metadata import register_track_metadata_processor
 
 def remove_sort_tags(tagger, metadata, release, track):
-    TAGS = ['albumartistsort', 'albumsort', 'artistsort', 'composersort', 'titlesort']
     for tag in TAGS:
         if tag in metadata:
             del metadata[tag]
